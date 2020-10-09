7a0519363a677aa4ed67c141492438e875cd62f3
Prog24 <kenta@suzuki.dev>
2020-10-09 09:06:36 +0900

    delete MonsterZoo.java (#16)

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
 public class Main {
-	static MonsterZoo pz = new MonsterZoo();
 
 	public static void main(String[] args) {
+		Player player = new Player();
+		Event event = new Event();
 
-		//1000ミリ秒（1秒）ずつ止まりながらpz.move()を呼び出し続ける
+		//1000ミリ秒（1秒）ずつ止まりながらevent.move()を呼び出し続ける
 		//手持ちのボールが無くなったら終了
 		while(true){
 			try {
 				Thread.sleep(1000);
-				if(pz.player.balls.count()>0){
-					pz.move();
+				if(player.balls.count()>0){
+					event.move(player);
 				}else{
 					break;
 				}
@@ -20,9 +21,9 @@ public class Main {
 
 		System.out.println("ボールがなくなった！");
 
-		for(int i=0;i<pz.player.monster.size();i++){
-			if(pz.player.monster.get(i)!=null){
-				System.out.println(pz.player.monster.get(i)+"を捕まえた．");
+		for(int i=0;i<player.monster.size();i++){
+			if(player.monster.get(i)!=null){
+				System.out.println(player.monster.get(i)+"を捕まえた．");
 			}
 		}
 	}
